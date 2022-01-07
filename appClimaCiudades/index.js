400c705c2a9030371a378afb13562110f8b34213
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2022-01-06 19:21:47 -0500

    funcionalidad de historial integrado, y fín de proyecto

--- appClimaCiudades/index.js
+++ appClimaCiudades/index.js
@@ -7,6 +7,7 @@ const {
 } = require("./helpers/inquirer");
 const ModeloCiudad = require("./models/busquedas");
 require("colors");
+console.clear();
 main();
 let opcion;
 async function main() {
@@ -23,22 +24,40 @@ async function main() {
 
         //creamos el menú para elegir cual es el que deseamos
         const id = await menuElegirLugar(arregloLugares);
+
+        if (id === "0") continue;
+
         const informacion = arregloLugares.find((l) => l.id === id);
+
+        //grabar bd
+        modeloCiudad.agregarHistorial(informacion);
+
+        modeloCiudad.lat = informacion.lat;
+        modeloCiudad.lon = informacion.lon;
+        const parametrosClima = await modeloCiudad.climaCiudad(
+          informacion.lat,
+          informacion.lon
+        );
 
         console.log("\nInformacion de la ciudad \n".green);
         console.log("Ciudad:", informacion.name);
         console.log("lat: ", informacion.lat);
         console.log("Lon: ", informacion.lon);
-        console.log("Temperatura: ");
-        console.log("Mínima: ");
-        console.log("Máxima: ");
+        console.log("Temperatura: ", parametrosClima.temp);
+        console.log("Mínima: ", parametrosClima.min);
+        console.log("Máxima: ", parametrosClima.max);
+        console.log("Como esta el clima: ", parametrosClima.desc);
 
         break;
 
       case 2:
-        break;
+        modeloCiudad.break;
 
       default:
+        modeloCiudad.leerBd();
+        modeloCiudad.historial.forEach((linea) => {
+          console.log(linea.name);
+        });
         break;
     }
     if (opcion !== 0) await pausaIteractiva();
--- appClimaCiudades/models/busquedas.js
+++ appClimaCiudades/models/busquedas.js
@@ -1,13 +1,30 @@
+const fs = require("fs");
 const { default: axios } = require("axios");
 
 class ModeloCiudad {
   historial = [];
+  lat;
+  lon;
+  path = "./db/base.json";
+
+  constructor() {
+    this.leerBd();
+  }
 
   get params() {
     return {
       access_token: process.env.MAPBOX_KEY,
       limit: 5,
       language: "es",
+    };
+  }
+  get paramsWeather() {
+    return {
+      lat: this.lat,
+      lon: this.lon,
+      appid: process.env.OPENWEATHER,
+      units: "metric",
+      lang: "es",
     };
   }
 
@@ -30,6 +47,52 @@ class ModeloCiudad {
     } catch (error) {
       return [];
     }
+  }
+
+  async climaCiudad(lat, lon) {
+    try {
+      const instancia = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+        params: this.paramsWeather,
+      });
+      const respuesta = await instancia.get();
+      const { main, weather } = respuesta.data;
+
+      return {
+        desc: weather[0].description,
+        min: main.temp_min,
+        max: main.temp_max,
+        temp: main.temp,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  agregarHistorial(nombre) {
+    if (this.historial.includes(nombre)) {
+      return;
+    }
+    this.historial.unshift(nombre);
+
+    //Guardar en la base
+    this.grabarBd();
+  }
+
+  grabarBd() {
+    const objetoHistorial = {
+      historial: this.historial,
+    };
+    fs.writeFileSync(this.path, JSON.stringify(objetoHistorial));
+  }
+
+  leerBd() {
+    if (!fs.existsSync(this.path)) {
+      return;
+    }
+    const info = fs.readFileSync(this.path, { encoding: "utf-8" });
+    const data = JSON.parse(info);
+    this.historial = data.historial;
   }
 }
 
