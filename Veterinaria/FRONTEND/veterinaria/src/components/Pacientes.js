55d98f5b5d554cb2990496ff72bfae356a660e37
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2022-01-11 20:03:57 -0500

    Retoques finales

--- Veterinaria/FRONTEND/veterinaria/src/components/Pacientes.js
+++ Veterinaria/FRONTEND/veterinaria/src/components/Pacientes.js
@@ -5,7 +5,7 @@ const Pacientes = ({ citas }) => {
   if (citas.length === 0) return null;
   return (
     <Fragment>
-      <h1 className="my-5">Administrador de Pacientes</h1>
+      <h1 className="my-5">veterinaria Egas</h1>
       <div className="container mt-5 py-5">
         <div className="row">
           <div className="col-12 mb-5 d-flex justify-content-center">
